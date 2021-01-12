b3ebf9a1e2436bc4e3c65c2c943b0a0279e3df2c
Thibault Monsel <thibaultmonsel@gmail.com>
2021-01-12 14:23:03 +0100

    remove work around for nn.CrossEntropyLoss() mini-batch problem

--- main.py
+++ main.py
@@ -33,33 +33,30 @@ for epoch in range(2):
     # training
     model.train() # mode "train" agit sur "dropout" ou "batchnorm"
     for batch_idx, data in enumerate(train_loader):
-        #temp workaround of cross_entropy mini_batch pb
-        if data.y.shape[0] == 64 : 
-            optimizer.zero_grad()
-            data = data.to(device)
-            out = model(data)
-            data.y = data.y.view(-1)
-            loss = loss_fn(out, data.y)
-            loss.backward()
-            optimizer.step()
-            if batch_idx %100 ==0:
-                print('epoch {} batch {} [{}/{}] training loss: {}'.format(epoch,batch_idx,batch_idx*len(data),
-                        len(train_loader.dataset),loss.item()))
+        optimizer.zero_grad()
+        data = data.to(device)
+        out = model(data)
+        data.y = data.y.view(-1)
+        loss = loss_fn(out, data.y)
+        loss.backward()
+        optimizer.step()
+        if batch_idx %100 ==0:
+            print('epoch {} batch {} [{}/{}] training loss: {}'.format(epoch,batch_idx,batch_idx*len(data),
+                    len(train_loader.dataset),loss.item()))
     
     # testing
     model.eval()
     correct, concat_prediction, concat_target = 0, torch.empty(0), torch.empty(0)
     with torch.no_grad():
         for batch_idx, data in enumerate(valid_loader):
-            if data.y.shape[0] == 64 : 
-                optimizer.zero_grad()
-                data = data.to(device)
-                data.y = data.y.flatten()
-                out = model(data)
-                loss = loss_fn(out, data.y)
-                prediction = out.argmax(dim=1, keepdim=True) 
-                concat_prediction = torch.cat((concat_prediction, prediction), 0)
-                concat_target = torch.cat((concat_target, data.y), 0)
-                correct += prediction.eq(data.y.view_as(prediction)).sum().item()
-        taux_classif = 100. * correct / len(test_loader.dataset)
-        print('Accuracy: {} --- ROC_AUC {} \n'.format(taux_classif, roc_auc_compute_fn(concat_prediction, concat_target)))
+            optimizer.zero_grad()
+            data = data.to(device)
+            data.y = data.y.flatten()
+            out = model(data)
+            loss = loss_fn(out, data.y)
+            prediction = out.argmax(dim=1, keepdim=True) 
+            concat_prediction = torch.cat((concat_prediction, prediction), 0)
+            concat_target = torch.cat((concat_target, data.y), 0)
+            correct += prediction.eq(data.y.view_as(prediction)).sum().item()
+    taux_classif = 100. * correct / len(test_loader.dataset)
+    print('Accuracy: {} --- ROC_AUC {} \n'.format(taux_classif, roc_auc_compute_fn(concat_prediction, concat_target)))
